57c29db32f75316e6949e1f81d0eb2b37b90d0e3
andreyonathan65 <andreyonathan65@gmail.com>
2025-11-08 03:13:16 +0000

    Implement simple Supabase call

--- lib/widgets/contact_list_widget.dart
+++ lib/widgets/contact_list_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myapp/providers/contact_provider.dart';
 import 'package:myapp/screens/manage_contact_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ContactListWidget extends StatefulWidget {
   const ContactListWidget({super.key});
@@ -11,9 +12,34 @@ class ContactListWidget extends StatefulWidget {
 }
 
 class _ContactListWidgetState extends State<ContactListWidget> {
+  final _future = Supabase.instance.client
+      .from('contacts')
+      .select();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return FutureBuilder(
+      future: _future,
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+
+        final contacts = snapshot.data!;
+
+        return ListView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            final contact = contacts[index];
+
+            return ListTile(
+              title: Text(contact['name']),
+              subtitle: Text(contact['email']),
+            );
+          }
+        );
+      }
+    );
   }
 }
 
